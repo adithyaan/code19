a99b6c19936cde312762aa708a694989bf199346
adithyaan <anirudhadi777@gmail.com>
2020-04-10 11:12:55 +0530

    drawer setup

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import { createDrawerNavigator } from "react-navigation-drawer";
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import signUp from './components/signup/signUp';
 import signIn from './components/signin/signIn';
 import Home from './components/home/home';
 
-const drawerNavigator = createDrawerNavigator({
-  Home: { screen: Home },
-});
+const drawerNavigator = createDrawerNavigator(
+  {
+    Home: {screen: Home},
+    test: {screen: Home},
+    test1: {screen: Home},
+  },
+  {
+    initialRouteName: 'Home',
+    drawerBackgroundColor: '#000000',
+    backBehavior: 'initialRoute',
+    activeBackgroundColor: '#ffffff',
+    contentOptions: {
+      inactiveTintColor: '#ffffff',
+    },
+  },
+);
 
 const AuthNavigator = createStackNavigator(
   {
@@ -18,10 +31,9 @@ const AuthNavigator = createStackNavigator(
   },
   {
     initialRouteName: 'Home',
-  }
+  },
 );
 
 const App = createAppContainer(AuthNavigator);
-
 
 export default App;
